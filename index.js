bb10febd47e4cabd955ed340a73243959174a95a
zippykitche <zippyk80@gmail.com>
2024-09-30 23:10:21 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result;
+  if (distance <= 400 ) {
+result = 'This one is on me!';
+  }
+else if (distance > 400 && distance <=2000) {
+  result = 'That will be twenty bucks.';
+}
+else if (distance > 2000 && distance <=2500) {
+  result = 'I will gladly take your thirty bucks.'; 
+}
+else if (distance > 2500) {
+  result = 'No can do.' 
+}
+  return result
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+  
+  case 'not as generous': 
+  return 'Thank you.';
+  
+ default: 
+  return 'Bye.'
+  
+  }
 }
